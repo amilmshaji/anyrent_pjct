87454fc054199601492a56455d8792c9810f0f27
amilmshaji <msamilmanadiyil2560@gmail.com>
2022-10-16 19:19:05 +0530

    lost password

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
     path('register/', views.register, name='register'),
     path('logout', views.logout, name='logout'),
+    path('forgotPassword/', views.forgotPassword, name='forgotPassword'),
+    path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
+    path('resetPassword/', views.resetPassword, name='resetPassword'),
 
 ]
