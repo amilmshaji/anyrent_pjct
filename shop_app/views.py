b3a77f14dccabbc39da27e8b847a0aee3c90fd4a
amilmshaji <msamilmanadiyil2560@gmail.com>
2022-10-16 19:03:30 +0530

    products visible

--- shop_app/views.py
+++ shop_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, redirect, render
 
-from products.models import Car_Product, House_Product
+from products.models import Car_Product, House_Product, Bike_Product, Furn_Product, Other_Product
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator, InvalidPage
 from django.db.models import Q
 # Create your views here.
@@ -14,6 +14,13 @@ def shop(request, category_slug=None):
     if category_slug != None:
         h_products = House_Product.objects.filter( is_available=True)
         c_products = Car_Product.objects.filter( is_available=True)
+        b_products = Bike_Product.objects.filter( is_available=True)
+        f_products = Furn_Product.objects.filter( is_available=True)
+        o_products = Other_Product.objects.filter( is_available=True)
+
+
+
+
 
         paginator = Paginator(products, 1)
         page = request.GET.get('page')
@@ -23,6 +30,10 @@ def shop(request, category_slug=None):
 
         h_products = House_Product.objects.all().filter(is_available=True).order_by('id')
         c_products = Car_Product.objects.all().filter(is_available=True).order_by('id')
+        b_products = Bike_Product.objects.all().filter(is_available=True).order_by('id')
+        f_products = Furn_Product.objects.all().filter(is_available=True).order_by('id')
+        o_products = Other_Product.objects.all().filter(is_available=True).order_by('id')
+
 
         paginator = Paginator(h_products, 9)
         page = request.GET.get('page')
@@ -32,7 +43,9 @@ def shop(request, category_slug=None):
     context = {
         'h_products' : h_products,
         'c_products': c_products,
-
+        'b_products': b_products,
+        'f_products': f_products,
+        'o_products': o_products,
         # 'h_products': paged_products,
         'product_count': product_count,
     }
